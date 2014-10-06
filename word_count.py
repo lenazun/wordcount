b4f532a71cc8b9142ecb94912a4cac486e7bf861
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 12:54:54 -0700

    MVP completed

--- word_count.py
+++ word_count.py
@@ -2,13 +2,17 @@ from sys import argv
 
 script, filename = argv
 
+punctuation = ('.', ',', '?', ':', ";", '"', "'")
+
 text = open(filename)
 passage = text.read()
+passage = passage.strip(punctuation)
 words = passage.split()
 
 word_dict = {}
 
 for word in words:
+
     if word in word_dict:
         word_dict[word] += 1
     else:
