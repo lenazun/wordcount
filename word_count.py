6cb6ec2fa66753c960a97c3b9bd5cef307f57dff
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 15:08:45 -0700

    removed punctuation

--- word_count.py
+++ word_count.py
@@ -2,16 +2,19 @@ from sys import argv
 
 script, filename = argv
 
-punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
 
 text = open(filename)
 passage = text.read()
 passage = passage.lower()
 words = passage.split()
 
+
+
 word_dict = {}
 
 for word in words:
+
+    word = word.strip("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
 
     if word in word_dict:
         word_dict[word] += 1
